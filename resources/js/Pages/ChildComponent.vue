e8fbe93e244f940cd32efce718da6fe22a3467e5
Cesar Febres <febrescesar7@gmail.com>
2024-02-07 17:28:32 -0400

    inertia vue and ts

---
+++ resources/js/Pages/ChildComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>{{ props.userTs.name }}</p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { User } from '@/User';
+
+
+const props = defineProps<{
+  userTs: User
+}>();
+</script>
+
